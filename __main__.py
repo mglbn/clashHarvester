42e457f582bee3d7fa30f424d60a031408650987
marius <mariusglauben@gmail.com>
2022-09-23 18:58:00 +0200

    basic request to api

--- __main__.py
+++ __main__.py
@@ -1,9 +1,23 @@
 import sys
+import os
+from urllib import response
+from dotenv import load_dotenv
+import requests
+
+def getplayer(tag):
+    response = requests.get(url= os.environ["BASEURL"]+tag,
+    headers= {"Accept": "application/json", "Authorization": "Bearer "+ os.environ["TOKEN"]})
+
+    #Do some error handling
+
+    return response.json()
+
 
 def main():
-    print("runnung")
+    print(getplayer("Y8YGQLJ0"))
     return 0
 
 
 if __name__=="__main__":
+    load_dotenv()
     sys.exit(main())
