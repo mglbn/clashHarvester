e8aad031d2d6c512560cf55255bd0909411262b7
marius <mariusglauben@gmail.com>
2022-09-26 01:30:04 +0200

    exception handling

--- __main__.py
+++ __main__.py
@@ -1,22 +1,62 @@
 import sys
 import os
-from urllib import response
+from urllib.error import HTTPError
 from dotenv import load_dotenv
 import requests
+import time
+import socket
 
-def getplayer(tag):
+
+class NoPlayerException(Exception):
+    pass
+
+def notifyonfailure(message):
+    print(message)
+    body = {
+            "chat_id": os.environ["CHATID"],
+            "text" : message
+    }
+    sent = False
+    tries = 0
+    while not sent and tries < 10:
+        try:
+            response = requests.post("https://api.telegram.org/bot"+os.environ["TELEGRAM"]+"/sendMessage",json=body).json()
+            sent = response["ok"]
+        except Exception as e:
+            tries+=1
+            time.sleep(2)
+    return sent
+
+
+def getPlayerData(tag):
     response = requests.get(url= os.environ["BASEURL"]+tag,
-    headers= {"Accept": "application/json", "Authorization": "Bearer "+ os.environ["TOKEN"]})
+        headers= {"Accept": "application/json", "Authorization": "Bearer "+ os.environ["TOKEN"]})
+    if not response.status_code==200:
+        raise HTTPError(response)
 
-    #Do some error handling
+    body= response.json()
 
-    return response.json()
+    if body.get("reason")=="notFound":
+        raise NoPlayerException("")
+    
+    if body.get("tag")== None:
+        raise Exception("smth wrong with body")
+
+    return body
 
 
 def main():
-    print(getplayer("Y8YGQLJ0"))
-    return 0
+    try:
+        print(getPlayerData("Y8YGQLJ0"))
+    except NoPlayerException:
+        pass
+    except Exception as e:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        ip = s.getsockname()[0]
 
+        notifyonfailure(ip+"\n"+str(e.with_traceback))
+    return 0
 
 if __name__=="__main__":
     load_dotenv()
