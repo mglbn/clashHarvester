6e48e147feb3b595c367512d4a4bbfd037d193fb
marius <mariusglauben@gmail.com>
2022-10-18 13:50:53 +0200

    sdlfksl

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-mkdir -p ~/.config/systemd/user
-cp ./clashHarvester.service ~/.config/systemd/user/ 
+mkdir -p ~/.config/systemd/user && cp ~/clashHarvester/clashHarvester.service ~/.config/systemd/user/ 
 systemctl --user enable clashHarvester
 loginctl enable-linger $USER
