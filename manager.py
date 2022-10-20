37f6b95dd7825707c34927d4267a9d5445b47107
marius <mariusglauben@gmail.com>
2022-10-20 13:42:59 +0200

    fehlerbehebung blabla

--- manager.py
+++ manager.py
@@ -1,12 +1,17 @@
 import requests
-
+import time
 _mngnt_server_url= "http://91.200.100.224:17823"
 
 def getNewWorkload():
-    
-    resp = requests.get(_mngnt_server_url + "/workload")
-    json = resp.json()
-    return json
+    while True:
+        try:
+            resp = requests.get(_mngnt_server_url + "/workload")
+            json = resp.json()
+            return json
+        except requests.JSONDecodeError:
+            print("Managementserver offline. Versuche erneut in 20s.")
+            time.sleep(20)
+            continue
 
 def confirm(oldtag):
     para = {"oldtag":oldtag}
